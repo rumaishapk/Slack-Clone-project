3660485f217925ec25732260a8169b8ab7c517c3
rumaishapk <rumaisharumi27@gmail.com>
2026-04-07 12:36:19 +0530

    create model,inngest implement

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,32 @@
-import express from "express"
+import express from "express";
 import { ENV } from "./config/env.js";
-
-
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
-
+app.use(express.json);
+app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   return res.send("hello");
 });
 
-console.log("mongo url:",ENV.MONGO_URL)
- app.listen(5001, () => {
-  console.log(ENV.PORT," Server is running on port 5001:",ENV.PORT);
-});
- 
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(" Server is running on port :", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:",error )
+    process.exit(1)
+  }
+};
+startServer()
+
+export default app;
